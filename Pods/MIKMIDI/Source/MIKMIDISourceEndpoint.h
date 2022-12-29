4fd5fc3e80fed547740b25966fd1b39752003923
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-29 03:20:59 -0500

    spelling: messages
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- Pods/MIKMIDI/Source/MIKMIDISourceEndpoint.h
+++ Pods/MIKMIDI/Source/MIKMIDISourceEndpoint.h
@@ -17,7 +17,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  *  Block used by various MIKMIDI APIs that deliver incoming MIDI messages.
  *
- *  @param source	The source endpoint from which MIDI messagse were received.
+ *  @param source	The source endpoint from which MIDI messages were received.
  *  @param commands	An NSArray containing received MIKMIDICommand instances.
  */
 typedef void(^MIKMIDIEventHandlerBlock)(MIKMIDISourceEndpoint *source, MIKArrayOf(MIKMIDICommand *) *commands); // commands in an array of MIKMIDICommands
