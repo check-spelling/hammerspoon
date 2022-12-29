5ac09daa2899006bccaf084a818aca8a07563274
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-29 03:32:46 -0500

    spelling: success
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- Pods/Sentry/Sources/SentryCrash/Recording/SentryCrash.h
+++ Pods/Sentry/Sources/SentryCrash/Recording/SentryCrash.h
@@ -39,7 +39,7 @@ typedef enum {
 
 typedef enum {
     SentryCrashCDeleteNever,
-    SentryCrashCDeleteOnSucess,
+    SentryCrashCDeleteOnSuccess,
     SentryCrashCDeleteAlways
 } SentryCrashCDeleteBehavior;
 
